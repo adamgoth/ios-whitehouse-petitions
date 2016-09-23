cac001587e41270a6311f91be4ab5cfb1d4cb84e
Adam Goth <adamgoth@gmail.com>
2016-09-23 07:29:41 -0600

    Initial Commit

---
+++ whitehouse-petitions/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  whitehouse-petitions
+//
+//  Created by Adam Goth on 9/23/16.
+//  Copyright © 2016 Adam Goth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
